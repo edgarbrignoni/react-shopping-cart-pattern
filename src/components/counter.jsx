072dca2de1c7adf4a2af1a561fbf59244c69a500
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-28 14:29:15 -0400

    Seeting Attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
@@ -8,14 +8,14 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
 
   formatCount() {
-    const {count} = this.state;
+    const { count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
