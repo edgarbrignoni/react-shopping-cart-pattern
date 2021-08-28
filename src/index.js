d69df01660d8c34580de43c5feedd93086fedb59
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-28 14:24:39 -0400

    Remove App definintion since it is not being use

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
 
 ReactDOM.render(
   <React.StrictMode>
     <Counter />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
