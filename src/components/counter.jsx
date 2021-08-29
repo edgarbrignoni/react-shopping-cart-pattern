730d011af3961c8aa7aeeec924459e969c474f1a
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-28 22:33:20 -0400

    Binding Events Handlers via Constructor

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,9 +5,15 @@ class Counter extends Component {
     count: 0,
   };
 
-  handleIncrement() {
-    console.log("Increment Clicked");
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
   }
+
+  handleIncrement() {
+    console.log("Increment Clicked", this);
+  }
+
   render() {
     return (
       <div>
