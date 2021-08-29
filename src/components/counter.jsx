aada803f19e6c7c728983129b8107735b6afa087
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-28 23:23:18 -0400

    Not using children

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,7 +14,7 @@ class Counter extends Component {
 
     return (
       <div>
-        {this.props.children}
+        <h4>{this.props.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement()}
