72b0b5a8240d5e06d9073754b24e1c8bc563ef20
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-29 14:52:22 -0400

    Replace with basic bootstrap template

--- src/index.js
+++ src/index.js
@@ -3,11 +3,5 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-// import Counters from "./components/counters";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
