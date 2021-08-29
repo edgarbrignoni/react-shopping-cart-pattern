091992565c99c2dc057411d7539163f18590c716
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-29 12:22:59 -0400

    Raising and handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,13 +14,18 @@ class Counter extends Component {
 
     return (
       <div>
-        <h4>{this.props.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
