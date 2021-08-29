19552a925f2ac2e96c0c225c1a09df48255a20a4
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-28 22:37:40 -0400

    Updating the State

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,13 +5,8 @@ class Counter extends Component {
     count: 0,
   };
 
-  // constructor() {
-  //   super();
-  //   this.handleIncrement = this.handleIncrement.bind(this);
-  // }
-
   handleIncrement = () => {
-    console.log("Increment Clicked", this);
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
