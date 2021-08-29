89be4fc9a3afe609d403324050bf8cf2192eff3f
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-28 22:02:11 -0400

    Remove the poluting code on the render method by creating a new method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,15 +6,18 @@ class Counter extends Component {
   };
 
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-
     return (
       <div>
-        <span className={classes}>{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
