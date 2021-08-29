411159bd9b7f5b034b21370d2a57371a6618ea22
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-29 15:50:28 -0400

    Destructuring arguments by using object destructuring to make the code cleaner

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,17 +2,21 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   render() {
+    const { counter, onIncrement, onDelete } = this.props;
+
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <span className={this.getBadgeClasses(counter)}>
+          {this.formatCount(counter)}
+        </span>
         <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
+          onClick={() => onIncrement(counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
+          onClick={() => onDelete(counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
@@ -21,14 +25,14 @@ class Counter extends Component {
     );
   }
 
-  getBadgeClasses() {
+  getBadgeClasses(counter) {
     let classes = "badge m-2 badge-";
-    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    classes += counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
-  formatCount() {
-    const { value } = this.props.counter;
+  formatCount(counter) {
+    const { value } = counter;
     return value === 0 ? "Zero" : value;
   }
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
 // Stateless Functional Component
-const NavBar = props => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href={"/"}>
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
