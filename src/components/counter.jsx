be8b4f9745083cfb070d04a10ddd4405ad7f9548
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-28 22:56:06 -0400

    Passing Event Arguments using Inline Function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,16 +10,12 @@ class Counter extends Component {
     this.setState({ count: this.state.count + 1 });
   };
 
-  doHandleIncrement = () => {
-    this.handleIncrement({ id: 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.doHandleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
