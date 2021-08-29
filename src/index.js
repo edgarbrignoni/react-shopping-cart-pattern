639c13edc8fed74ddc104742d9e56a681fac5608
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-28 23:05:16 -0400

    Composing Components

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
+import Counters from "./components/counters";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <Counters />
   </React.StrictMode>,
   document.getElementById("root")
 );
