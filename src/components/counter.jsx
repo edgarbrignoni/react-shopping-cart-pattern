ec56ef91dced36c7f0e0793c426c94364e5ad4ea
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-28 22:42:54 -0400

    Passing Event Arguments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,8 +5,13 @@ class Counter extends Component {
     count: 0,
   };
 
-  handleIncrement = () => {
+  handleIncrement = (product) => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
+  };
+
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1 });
   };
 
   render() {
@@ -14,7 +19,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={this.doHandleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
