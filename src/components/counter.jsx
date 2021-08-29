1fb8fe22cd55357e0f375191105a174aac9dbdef
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-28 21:51:10 -0400

    Delete image tag

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,13 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200",
   };
 
   render() {
     return (
       <div>
-        <img src={this.state.imageUrl} alt="" />
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
       </div>
     );
   }
