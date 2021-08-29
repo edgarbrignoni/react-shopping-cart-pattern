5b67166875874845fae6de2689a218ad7761c44d
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-28 21:53:46 -0400

    Apply style attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,10 +5,17 @@ class Counter extends Component {
     count: 0,
   };
 
+  styles = {
+    fontSize: 50,
+    fontWeight: "bold",
+  };
+
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
