af95153a600f956ae7c9eb75f7e1e3eedd15bad2
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-08-29 16:57:06 -0400

    Set all values to 0 on mount

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 class App extends Component {
   state = {
     counters: [
-      { id: 1, value: 4 },
+      { id: 1, value: 0 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
