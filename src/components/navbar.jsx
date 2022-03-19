045fa0f0d3b47a98d5f0bcf2c3a098560cf1fcef
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-03-19 19:50:50 -0400

    demo gh-pages

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,10 +5,8 @@ const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <span className="navbar-brand">
-        Navbar
-        <span className="badge badge-pill badge-secondary m-2">
-          {totalCounters}
-        </span>
+        Navegar
+        <span className="badge badge-pill badge-secondary m-2">{totalCounters}</span>
       </span>
     </nav>
   );
